b3f3f16b8a95bc15416eec2126379fe4678614d0
ahmedshalaby03 <jockernocker9@gmail.com>
2026-07-25 07:05:52 +0300

    Initial commit

---
+++ Backend/skinet/skinet/Controllers/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public StoreContext Context { get; }
+        public ProductsController(StoreContext Context)
+        {
+            this.Context = Context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await Context.Products.ToListAsync();
+            if (products is null) return NotFound();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var p = await Context.Products.FindAsync(id);
+            if (p is null) return NotFound();
+            return p;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            Context.Products.Add(product);
+            await Context.SaveChangesAsync();
+            return product;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id!= id || !ProductExist(id))
+                return BadRequest("Cannot update product");
+
+            Context.Entry(product).State = EntityState.Modified;
+            await Context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var p = await Context.Products.FindAsync(id);
+            if (p == null) return NotFound();
+            Context.Products.Remove(p);
+            await Context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductExist(int id)
+        {
+            return Context.Products.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ Backend/skinet/skinet/Program.cs
@@ -0,0 +1,36 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace skinet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
